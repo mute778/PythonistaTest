59f7c7af3644c7c67e2a65cb6f4f03ff1d03ec29
mute <mute.develop@gmail.com>
2019-04-27 01:19:19 +0000

    test

--- MuDice.py
+++ MuDice.py
@@ -1,6 +1,30 @@
 import ui
 
+PLAYER_01 = 0
+PLAYER_02 = 1
+DAMAGE = 0
+RECOVERY = 1
+Player01LP = 2500
+Player02LP = 2500
+
 v = ui.load_view()
 v.present('sheet')
 
-player00LpLabel = sender.superview['label1']
+O_p1lp = v['label4']
+O_p2lp = v['label6']
+
+def update_player_life():
+	O_p1lp.text = "{:,}".format(Player01LP)
+	O_p2lp.text = "{:,}".format(Player02LP)
+	
+def change_life_value(target, value):
+	if target == PLAYER_01:
+		Player01LP = Player01LP + value
+	elif target == PLAYER_02:
+		Player02LP = Player02LP + value
+	update_player_life()
+		
+def init_setting():
+	update_player_life()
+	
+init_setting()
